feea655efb685573770d053284dc2a6aa4d5700a
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-21 21:15:27 +0200

    start

--- src/index.js
+++ src/index.js
@@ -6,3 +6,18 @@ import {fetchCountries} from './index.js';
 
 // ?fields=name.official,capital,population,flags.svg,languages
 
+refs = {
+input:document.querySelector("#search-box")
+}
+
+const createCardCounties = 
+`<li class= "name-country">${dwq}</li>
+<img class ='img-gallery'
+src = ${dsa}
+alt =${dsad}>
+</li>`
+
+
+
+// gallaryEl.insertAdjacentHTML("afterbegin", createImage)
+// console.log(createImage)
