3d0f84ee5842edc711bc99255d6b99179ced9dba
Valerii219 <119235568+Valerii219@users.noreply.github.com>
2023-05-24 20:20:52 +0000

    Deploying to gh-pages from @ Valerii219/goit-js-hw-10@c72934bec70653832a8d8481ad1a23215dbdc6e6 🚀

--- index.da2741ac.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".breed-select");e.addEventListener("change",(function(t){e.value;fetch("https://api.thecatapi.com/v1/breeds?key=live_PeQIm7clvMSlGltRB8qyWPC8h4vJpnzq15HYHXZFt4wayVOb6jvRMCRXSexktexb").then((e=>{if(console.log(e),!e.ok)throw new Error(e.statusText);return e.json()})).then((t=>{e.insertAdjacentHTML("beforeend",t.breeds.map((({id:e})=>'<option value="${id}">${id}</option>)'.join(""))))})).catch((e=>console.error(e)))})),console.dir(e);
-//# sourceMappingURL=index.da2741ac.js.map
