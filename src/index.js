4557f88c83fff04868ef54c04d33fb288ebd697e
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-24 18:58:44 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 import {fetchBreeds} from './cat-api.js';
 
-
 const select = document.querySelector('.breed-select');
 select.addEventListener('change', functionSelect);
 console.dir(select);
 
-functionSelect()
+function functionSelect(evt) {
+    const rrr = select.value;
 
-function functionSelect(evt){
-    const {id} = select.value;
-
-fetchBreeds()
-.then(data => select.insertAdjacentHTML = console.log(select.insertAdjacentHTML('beforeend', createMarkup(data.breeds))  ))
-.catch(err => console.error(err)
-)
-
+    fetchBreeds()
+    .then(data => {
+        select.insertAdjacentHTML('beforeend', createMarkup(data.breeds));
+    })
+    .catch(err => console.error(err));
 }
+
 function createMarkup(arr) {
-    return arr.map(({id}) => 
-    `<option value="ddd">"ggg"</option>`)
-    .join('')
-   
+    return arr.map(({id}) => '<option value="${id}">${id}</option>)'.join(''))
 }
