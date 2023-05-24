c72934bec70653832a8d8481ad1a23215dbdc6e6
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-24 22:20:28 +0200

    www

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,5 @@
 
-function fetchBreeds(id = ""){
+function fetchBreeds(){
     const BASE_URL = 'https://api.thecatapi.com/v1';
     const API_KEY = 'live_PeQIm7clvMSlGltRB8qyWPC8h4vJpnzq15HYHXZFt4wayVOb6jvRMCRXSexktexb';
     return fetch(`${BASE_URL}/breeds?key=${API_KEY}`)
@@ -9,6 +9,19 @@ function fetchBreeds(id = ""){
     }
     return resp.json()
     })
-    }
+    };
    
-    export {fetchBreeds};
+    export {fetchBreeds, fetchCatByBreed};
+
+   function fetchCatByBreed(breedId){
+const SECOND_URL = 'https://api.thecatapi.com/v1/images/search';
+const API_KEY = 'live_PeQIm7clvMSlGltRB8qyWPC8h4vJpnzq15HYHXZFt4wayVOb6jvRMCRXSexktexb';
+return fetch(`${SECOND_URL}?key=${API_KEY}/breed_ids=beng/`)
+    .then(resp =>{console.log(resp)
+    if(!resp.ok){
+    throw new Error(resp.statusText)
+    }
+    return resp.json()
+    })
+
+    }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,65 @@
-import {fetchBreeds} from './cat-api.js';
+import {fetchBreeds,fetchCatByBreed} from './cat-api.js';
 
 const select = document.querySelector('.breed-select');
+const cotInfo = document.querySelector('.cat-info')
+
 select.addEventListener('change', functionSelect);
-console.dir(select);
 
-function functionSelect(evt) {
-    const rrr = select.value;
-
-    fetchBreeds()
+console.dir(select.selectedIndex);
+function functionSelect(e) {
+    
+    const selectedOptionValue = select.value;
+    
+    if (selectedOptionValue) {
+        fetchBreeds()
     .then(data => {
-        select.insertAdjacentHTML('beforeend', createMarkup(data.breeds));
+    const markup = createMarkup(data);
+    select.innerHTML = markup;
     })
-    .catch(err => console.error(err));
+    .catch(err => {
+    console.error(err);
+    });
+    }
+    
+    
 }
+fetchBreeds()
+.then(data => {
+const markup = createMarkup(data);
+select.innerHTML = markup;
+})
+.catch(err => {
+console.error(err);
+});
+functionSelect();
 
 function createMarkup(arr) {
-    return arr.map(({id}) => '<option value="${id}">${id}</option>)'.join(''))
+return arr
+    .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
+    .join('');
+}
+fetchCatByBreed()
+catInfo()
+function catInfo() {
+    fetchCatByBreed()
+    .then(data => {
+        const markupInfo = createMarkupInfo(data);
+        cotInfo.innerHTML = markupInfo;
+        })
+        .catch(err => {
+        console.error(err);
+        });
+}
+fetchCatByBreed(select.value)
+.then(data => {
+    const markupInfo = createMarkupInfo(data);
+    cotInfo.innerHTML = markupInfo;
+    })
+    .catch(err => {
+    console.error(err);
+    });
+function createMarkupInfo(arr){
+    return arr
+    .map(breed => `<ul><img src ='${breed.url}'  alt = ><li>'${breed.id}'</li><li>'${breed.description}'</li><li>'${breed.temperament}'</li></ul>`)
+    .join('');
 }
