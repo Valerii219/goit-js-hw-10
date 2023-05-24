0da8f32d636bff2bd1eb0711fa6f486ce20781db
Valerii219 <119235568+Valerii219@users.noreply.github.com>
2023-05-24 16:59:14 +0000

    Deploying to gh-pages from @ Valerii219/goit-js-hw-10@4557f88c83fff04868ef54c04d33fb288ebd697e 🚀

--- index.f0d281d0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".breed-select");function t(t){const{id:o}=e.value;fetch("https://api.thecatapi.com/v1/breeds?key=live_PeQIm7clvMSlGltRB8qyWPC8h4vJpnzq15HYHXZFt4wayVOb6jvRMCRXSexktexb").then((e=>{if(console.log(e),!e.ok)throw new Error(e.statusText);return e.json()})).then((t=>e.insertAdjacentHTML=console.log(e.insertAdjacentHTML("beforeend",t.breeds.map((({id:e})=>'<option value="ddd">"ggg"</option>')).join(""))))).catch((e=>console.error(e)))}e.addEventListener("change",t),console.dir(e),t();
-//# sourceMappingURL=index.f0d281d0.js.map
