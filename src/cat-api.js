491d78f909552e76297af282424a99d9831313a7
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-27 21:45:28 +0200

    save changes

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,32 +2,35 @@ export {fetchBreeds, fetchCatByBreed};
 const API_KEY = 'live_PeQIm7clvMSlGltRB8qyWPC8h4vJpnzq15HYHXZFt4wayVOb6jvRMCRXSexktexb';
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const SECOND_URL = 'https://api.thecatapi.com/v1/images/search';
-
+let catId;
 function fetchBreeds(){
   
     return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`)
-    .then(resp =>{console.log(resp)
+    .then(resp =>{
     if(!resp.ok){
     throw new Error(resp.statusText)
     }
     return resp.json()
     })
+    
     .then(data => {
         return data;
       })
       .catch(error => {
         console.error(error);
-      });
-    };
+      })
 
+    };
     
+
     function fetchCatByBreed(breedId) {
       
-      return fetch(`https://api.thecatapi.com/v1/images/0XYvRd7oD`)
+      
+      return fetch(`${SECOND_URL}?api_key=${API_KEY}&breed_ids=${breedId}`)
         .then(resp => {
           if (!resp.ok) {
             throw new Error(resp.statusText);
-          }
+          }console.log(resp);
           return resp.json();
         })
         .then(data => {
--- src/index.js
+++ src/index.js
@@ -1,54 +1,56 @@
-import {fetchBreeds,fetchCatByBreed} from './cat-api.js';
+import {fetchBreeds, fetchCatByBreed} from './cat-api.js';
 
 const select = document.querySelector('.breed-select');
-const cotInfo = document.querySelector('.cat-info')
+const catInfo = document.querySelector('.cat-info');
+const load = document.querySelector('.loader')
+// load.addEventListener()
 
-select.addEventListener('change', functionSelect);
+
+
+select.addEventListener('change', () => {
+    const selectedOption = select.options[select.selectedIndex];
+    const selectedValue = selectedOption.value;
+
+fetchCatByBreed(selectedValue)
+.then(data =>{
+    const markupInfo = createMarkupInfo(data);
+    catInfo.innerHTML = markupInfo;})
+    .catch(err => {
+        console.error(err);
+        }); 
+})
+
 
 
 fetchBreeds()
 .then(data => {
-const markup = createMarkup(data);
-select.innerHTML = markup;
-})
-.catch(err => {
-console.error(err);
-});
+    const markup = createMarkup(data);
+    select.innerHTML = markup;
+    })
+    .catch(err => {
+    console.error(err);
+    });
 
-function functionSelect() {
-const selectedOptionValue = select.value;
-if (selectedOptionValue){
-    fetchCatByBreed(select.value)
-    .then(data => {
-        const markupInfo = `<ul>
-        <img src="${breed.url}" alt="${breed.name}">
-        <li>${breed.id}</li>
-        <li>${breed.description}</li>
-        <li>${breed.temperament}</li>
-      </ul>`;
-      cotInfo.insertAdjacentElement = ('beforeend', markupInfo );
-        })
-        .catch(err => {
-        console.error(err);
-        });
-    }
-}
 
 function createMarkup(arr) {
+
 return arr
     .map(breed => `<option value="${breed.id}">${breed.name}</option>
     `)
-    .join('');}
-
-function createMarkupInfo(arr){
-    console.log(arr);
-    return arr
-    .map(breed => `<ul>
-      <img src="${breed.url}" alt="${breed.name}">
-      <li>${breed.id}</li>
-      <li>${breed.description}</li>
-      <li>${breed.temperament}</li>
-    </ul>`)
     .join('');
 }
 
+
+
+
+function createMarkupInfo(ar){
+    
+    return ar
+    .map(breed => `<ul>
+      <img src="${breed.url}" alt="${breed.name}">
+      <li>${breed.breeds[0].name}</li>
+      <li>${breed.breeds[0].description}</li>
+      <li>${breed.breeds[0].temperament}</li>
+    </ul>`)
+    .join('');
+}
