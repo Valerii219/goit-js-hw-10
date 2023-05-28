465eff2979a5a4ebf773ea4564cdf71754686b1b
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-28 11:14:39 +0200

    + load

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,7 @@
 export {fetchBreeds, fetchCatByBreed};
 const API_KEY = 'live_PeQIm7clvMSlGltRB8qyWPC8h4vJpnzq15HYHXZFt4wayVOb6jvRMCRXSexktexb';
 const BASE_URL = 'https://api.thecatapi.com/v1';
-const SECOND_URL = 'https://api.thecatapi.com/v1/images/search';
+const SECOND_URL = 'https://api.thecatapi.com/v1/images/search111';
 let catId;
 function fetchBreeds(){
   
@@ -12,25 +12,20 @@ function fetchBreeds(){
     }
     return resp.json()
     })
-    
     .then(data => {
         return data;
       })
       .catch(error => {
         console.error(error);
       })
-
     };
     
-
     function fetchCatByBreed(breedId) {
-      
-      
       return fetch(`${SECOND_URL}?api_key=${API_KEY}&breed_ids=${breedId}`)
         .then(resp => {
           if (!resp.ok) {
             throw new Error(resp.statusText);
-          }console.log(resp);
+          };
           return resp.json();
         })
         .then(data => {
--- src/index.js
+++ src/index.js
@@ -3,71 +3,88 @@ import {fetchBreeds, fetchCatByBreed} from './cat-api.js';
 const select = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const load = document.querySelector('p.loader');
-console.log(load);
-// load.textContent = 'jdsjfd';
+const error = document.querySelector('.error');
+
+
 
 function showLoader(){
-    load.style.display = 'block'}
+    load.style.display = 'block';
+}
 
-    function hideLoader(){
-        load.style.display = 'none'
-    }
-    fetchData()
-    function fetchData() {
-        showLoader(); 
-        fetchBreeds();
-        hideLoader();
-    }
-        
-        
+function hideLoader(){
+load.style.display = 'none';
+}
 
+function hideElements(){
+select.style.display = 'none';
+catInfo.style.display = 'none'}
 
- select.addEventListener('change', () => {
-    const selectedOption = select.options[select.selectedIndex];
-    const selectedValue = selectedOption.value;
- 
+function showElements(){
+select.style.display = 'block';
+catInfo.style.display = 'block'}
 
-fetchCatByBreed(selectedValue)
-.then(data =>{
-    const markupInfo = createMarkupInfo(data);
-    catInfo.innerHTML = markupInfo;})
-    .catch(err => {
-        console.error(err);
-        }); 
-        
-})
-
-
-
+function workLoader() {
+    
+showLoader();
 fetchBreeds()
 .then(data => {
     const markup = createMarkup(data);
     select.innerHTML = markup;
     })
     .catch(err => {
-    console.error(err);
-    });
+       if(error.style.display = 'block') {
+        hideLoader()
+       }
+        
+        
+    })
+.finally(()=>{
+ hideLoader()
+ })
+ 
+    }
+    workLoader()
+    
+ select.addEventListener('change', () => {
+    
+    const selectedOption = select.options[select.selectedIndex];
+    const selectedValue = selectedOption.value;
+    
+    
+ showLoader()
+ hideElements()
+fetchCatByBreed(selectedValue)
+.then(data =>{
+    const markupInfo = createMarkupInfo(data);
+    catInfo.innerHTML = markupInfo;})
+    .catch(err => { if(error.style.display = 'block') {
+        hideLoader()
+       }
+    
+        
+        }).finally(()=>{
+ hideLoader();
+ showElements()
+ 
+        })
+})
+
 
 
 function createMarkup(arr) {
-
 return arr
     .map(breed => `<option value="${breed.id}">${breed.name}</option>
     `)
     .join('');
 }
 
-
-
-
 function createMarkupInfo(ar){
-    
     return ar
     .map(breed => `<ul>
-      <img src="${breed.url}" alt="${breed.name}">
-      <li>${breed.breeds[0].name}</li>
-      <li>${breed.breeds[0].description}</li>
-      <li>${breed.breeds[0].temperament}</li>
+    <img src="${breed.url}" alt="${breed.name}">
+    <li>${breed.breeds[0].name}</li>
+    <li>${breed.breeds[0].description}</li>
+    <li>${breed.breeds[0].temperament}</li>
     </ul>`)
     .join('');
 }
