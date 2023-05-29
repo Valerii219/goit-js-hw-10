48a3494d5ddf1453b51574c3d7feeca1120ffc88
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-29 19:08:08 +0200

    32

--- src/index.js
+++ src/index.js
@@ -32,8 +32,9 @@ Notiflix.Loading.standard(`${load.textContent}`);
 fetchBreeds().then(data => {
   const markup = createMarkup(data);
   select.innerHTML = markup;
-});
-Notiflix.Loading.remove(500);
+})
+Notiflix.Loading.remove(500)
+
 
 select.addEventListener('change', () => {
   const selectedOption = select.options[select.selectedIndex];
