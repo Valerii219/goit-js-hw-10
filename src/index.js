586cb6c6f9053b4e7ec5e332b0d41506a3129f09
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-05-29 19:22:53 +0200

    32

--- src/index.js
+++ src/index.js
@@ -28,12 +28,12 @@ function showElements() {
   catInfo.style.display = 'block';
 }
 
-Notiflix.Loading.standard(`${load.textContent}`);
+Notiflix.Loading.standard(`${load.textContent}`)
 fetchBreeds().then(data => {
   const markup = createMarkup(data);
   select.innerHTML = markup;
 });
-Notiflix.Loading.remove(500);
+Notiflix.Loading.remove(500)
 
 select.addEventListener('change', () => {
   const selectedOption = select.options[select.selectedIndex];
